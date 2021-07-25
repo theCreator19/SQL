5ad3636dde285fecf4a5c1dd9d334dd2d712a13d
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-07-25 22:04:46 +0530

    Create Weather Observation Station 14.sql

---
+++ Weather Observation Station 14.sql
@@ -0,0 +1,12 @@
+/*
+Weather Observation Station 14
+*/
+
+/*
+HackerRank Problem Solved using MYSQL.
+*/
+
+/*
+Enter your query here.
+*/
+select round(max(LAT_N),4) from STATION where LAT_N < 137.2345
