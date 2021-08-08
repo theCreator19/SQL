5471a48521a48742414ccbfbd9d699860c8ffccf
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-08-08 23:01:50 +0530

    Create Weather Observation Station 20.sql

---
+++ Weather Observation Station 20.sql
@@ -0,0 +1,13 @@
+/*
+Weather Observation Station 20
+*/
+
+/*
+HackerRank Problem Solved using Oracle.
+*/
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select round(MEDIAN(LAT_N),4) from STATION;
